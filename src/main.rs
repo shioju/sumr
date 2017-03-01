c15506f1008425f47373fb281efbe20e8ab73333
Sheng Hau <shioju12@gmail.com>
2017-03-01 11:18:56 +0800

    Returns total build time for dependent builds
    - take in base_url, build_id, username and password arguments

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,12 @@ use docopt::Docopt;
 use reqwest::Client;
 use reqwest::header::{Headers, Accept, qitem, Authorization, Basic};
 use mime::{Mime, Attr, SubLevel, TopLevel, Value};
-use rustc_serialize::json;
 use std::io::Read;
 
 const USAGE: &'static str = "
 sumr
 Usage:
-  sumr [options] <username> <password> <base_url> <build_id>
+  sumr [options] <base-url> <build-id> <username> <password>
   sumr -h | --help
 Options:
   -h --help                         Show this screen.
@@ -30,20 +29,80 @@ struct Args {
     arg_build_id: String,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Builds {
+    count: u32,
+    href: String,
+    build: Vec<Build>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Build {
+    id: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct BuildStatistics {
+    property: Vec<BuildProperties>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct BuildProperties {
+    name: String,
+    value: u32,
+}
+
 fn main() {
     let args: Args = Docopt::new(USAGE)
         .and_then(|d| d.decode())
         .unwrap_or_else(|e| e.exit());
 
-    println!("{:?}", args);
-    let url = args.arg_base_url + "/app/rest/latest/builds?locator=snapshotDependency:(to:(id:" + &args.arg_build_id + "),includeInitial:true),defaultFilter:false";
+    let builds_ids = get_dependent_builds(&args.arg_base_url, &args.arg_build_id, &args.arg_username, &args.arg_password).unwrap();
+
+    let mut total_build_time = 0;
+
+    for id in builds_ids {
+      total_build_time += get_build_time(&args.arg_base_url, &id.to_string(), &args.arg_username, &args.arg_password).unwrap();
+    }
+
+    println!("total build time = {}", total_build_time);
+}
+
+fn get_build_time(base_url: &str, build_id: &str, username: &str, password: &str) -> Option<u32> {
+    let url = base_url.to_string() + "/app/rest/latest/builds/id:" + build_id + "/statistics";
+    // println!("url for build time = {}", url);
+
     let mut response = get(&*url,
-                       &*args.arg_username,
-                       &*args.arg_password).map_err(|err| err.to_string()).unwrap();
+                           username,
+                           password).map_err(|err| err.to_string()).unwrap();
 
     let mut json_string = String::new();
     response.read_to_string(&mut json_string).map_err(|err| err.to_string()).unwrap();
-    println!("{}", json_string);
+    let deserialized: BuildStatistics = serde_json::from_str(&json_string).unwrap();
+
+    let build_time : Vec<u32> = deserialized.property.iter().filter(|prop| prop.name=="BuildDurationNetTime")
+                                                               .map(|prop| prop.value)
+                                                               .collect();
+
+    Some(build_time[0])
+}
+
+fn get_dependent_builds(base_url: &str, build_id: &str, username: &str, password: &str) -> Option<Vec<u32>> {
+    let url = base_url.to_string() + "/app/rest/latest/builds?locator=snapshotDependency:(to:(id:" + build_id + "),includeInitial:true),defaultFilter:false";
+    // println!("url for dependent builds = {}", url);
+
+    let mut response = get(&*url,
+                           username,
+                           password).map_err(|err| err.to_string()).unwrap();
+
+    let mut json_string = String::new();
+    response.read_to_string(&mut json_string).map_err(|err| err.to_string()).unwrap();
+    let deserialized: Builds = serde_json::from_str(&json_string).unwrap();
+    // println!("json = {:?}", deserialized);
+
+    let builds = deserialized.build.iter().map(|build| build.id).collect();
+
+    Some(builds)
 }
 
 fn get(url: &str, username: &str, password: &str) -> Result<reqwest::Response, reqwest::Error> {
