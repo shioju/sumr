7b39253dfd65361114cbb7e9560198afb9fda069
Sheng Hau <shioju12@gmail.com>
2017-03-01 12:59:59 +0800

    Reuse client connection pool

--- src/main.rs
+++ src/main.rs
@@ -57,74 +57,88 @@ fn main() {
         .and_then(|d| d.decode())
         .unwrap_or_else(|e| e.exit());
 
-    let builds_ids = get_dependent_builds(&args.arg_base_url, &args.arg_build_id, &args.arg_username, &args.arg_password).unwrap();
+    let client = Client::new().unwrap();
+
+    let builds_ids = get_dependent_builds(&client,
+                                          &args.arg_base_url,
+                                          &args.arg_build_id,
+                                          &args.arg_username,
+                                          &args.arg_password)
+        .unwrap();
 
     let mut total_build_time = 0;
 
     for id in builds_ids {
-      total_build_time += get_build_time(&args.arg_base_url, &id.to_string(), &args.arg_username, &args.arg_password).unwrap();
+        total_build_time += get_build_time(&client,
+                                           &args.arg_base_url,
+                                           &id.to_string(),
+                                           &args.arg_username,
+                                           &args.arg_password)
+            .unwrap();
     }
 
     println!("total build time = {}", total_build_time);
 }
 
-fn get_build_time(base_url: &str, build_id: &str, username: &str, password: &str) -> Option<u32> {
+fn get_build_time(client: &Client,
+                  base_url: &str,
+                  build_id: &str,
+                  username: &str,
+                  password: &str)
+                  -> Result<u32, String> {
     let url = base_url.to_string() + "/app/rest/latest/builds/id:" + build_id + "/statistics";
-    // println!("url for build time = {}", url);
 
-    let mut response = get(&*url,
-                           username,
-                           password).map_err(|err| err.to_string()).unwrap();
+    let mut response = get(client, &*url, username, password).map_err(|err| err.to_string())?;
 
     let mut json_string = String::new();
-    response.read_to_string(&mut json_string).map_err(|err| err.to_string()).unwrap();
-    let deserialized: BuildStatistics = serde_json::from_str(&json_string).unwrap();
+    response.read_to_string(&mut json_string).map_err(|err| err.to_string())?;
+    let deserialized: BuildStatistics =
+        serde_json::from_str(&json_string).map_err(|err| err.to_string())?;
 
-    let build_time : Vec<u32> = deserialized.property.iter().filter(|prop| prop.name=="BuildDurationNetTime")
-                                                               .map(|prop| prop.value)
-                                                               .collect();
+    let build_time: Vec<u32> = deserialized.property
+        .iter()
+        .filter(|prop| prop.name == "BuildDurationNetTime")
+        .map(|prop| prop.value)
+        .collect();
 
-    Some(build_time[0])
+    Ok(build_time[0])
 }
 
-fn get_dependent_builds(base_url: &str, build_id: &str, username: &str, password: &str) -> Option<Vec<u32>> {
-    let url = base_url.to_string() + "/app/rest/latest/builds?locator=snapshotDependency:(to:(id:" + build_id + "),includeInitial:true),defaultFilter:false";
-    // println!("url for dependent builds = {}", url);
+fn get_dependent_builds(client: &Client,
+                        base_url: &str,
+                        build_id: &str,
+                        username: &str,
+                        password: &str)
+                        -> Result<Vec<u32>, String> {
+    let url = base_url.to_string() + "/app/rest/latest/builds?locator=snapshotDependency:(to:(id:" +
+              build_id + "),includeInitial:true),defaultFilter:false";
 
-    let mut response = get(&*url,
-                           username,
-                           password).map_err(|err| err.to_string()).unwrap();
+    let mut response = get(client, &*url, username, password).map_err(|err| err.to_string())?;
 
     let mut json_string = String::new();
-    response.read_to_string(&mut json_string).map_err(|err| err.to_string()).unwrap();
-    let deserialized: Builds = serde_json::from_str(&json_string).unwrap();
-    // println!("json = {:?}", deserialized);
+    response.read_to_string(&mut json_string).map_err(|err| err.to_string())?;
+    let deserialized: Builds = serde_json::from_str(&json_string).map_err(|err| err.to_string())?;
 
     let builds = deserialized.build.iter().map(|build| build.id).collect();
 
-    Some(builds)
+    Ok(builds)
 }
 
-fn get(url: &str, username: &str, password: &str) -> Result<reqwest::Response, reqwest::Error> {
-  let mut headers = Headers::new();
-  headers.set(
-    Accept(vec![
-        qitem(Mime(TopLevel::Application, SubLevel::Json,
-                   vec![(Attr::Charset, Value::Utf8)])),
-    ])
-  );
-  headers.set(
-     Authorization(
-         Basic {
-             username: username.to_string(),
-             password: Some(password.to_string())
-         }
-     )
-  );
-  let client = Client::new()?;
-  let request_builder = client.get(url);
-  let request_builder = request_builder.headers(headers);
+fn get(client: &Client,
+       url: &str,
+       username: &str,
+       password: &str)
+       -> Result<reqwest::Response, reqwest::Error> {
+    let mut headers = Headers::new();
+    headers.set(Accept(vec![qitem(Mime(TopLevel::Application,
+                                       SubLevel::Json,
+                                       vec![(Attr::Charset, Value::Utf8)]))]));
+    headers.set(Authorization(Basic {
+        username: username.to_string(),
+        password: Some(password.to_string()),
+    }));
+    let request_builder = (*client).get(url);
+    let request_builder = request_builder.headers(headers);
 
-  let result = request_builder.send();
-  return result;
+    request_builder.send()
 }
